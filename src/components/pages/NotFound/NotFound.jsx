f5a82626d02cc9cc2cc887e716aad4149a49616a
araksya97 <[badalyan.araksya97@gmail.com]>
2020-12-17 15:27:01 +0400

    .

--- src/components/pages/NotFound/NotFound.jsx
+++ src/components/pages/NotFound/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import styles from './NotFoundStyle.module.css'
 export default function About() {
 
     return (
-       <div>
-           <h1>Error 404</h1>
+       <div className={styles.notfound} >
+           <h1>oops!!</h1>
+            <p className={styles.notfoundel}>Page not found</p>
        </div>
     )
 };
--- src/components/pages/SingleTask/SingleTask.jsx
+++ src/components/pages/SingleTask/SingleTask.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import {formatDate} from '../../../helpers/utils'
+import { formatDate } from '../../../helpers/utils'
 import Spinner from '../Spinner/Spinner'
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
+import EditTaskModal from '../../EditTaskModal/EditTaskModal'
+import styles from './SingleTaskStyles.module.css'
 
 export default class SingleTask extends React.PureComponent {
     state = {
-        singletask: null
+        singletask: null,
+        openeditTask: false
     };
     componentDidMount() {
         const taskId = this.props.match.params.id
         fetch(`http://localhost:3001/task/${taskId}`, {
-            method: 'PUT',
+            method: 'GET',
             headers: {
                 "Content-Type": "application/json"
             },
@@ -28,21 +34,98 @@ export default class SingleTask extends React.PureComponent {
                 console.log("ERROR")
             });
     };
+
+    onRemove = () => {
+        const taskId = this.state.singletask._id
+        fetch(`http://localhost:3001/task/${taskId}`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+            .then((res) => res.json())
+            .then(response => {
+
+                if (response.error) {
+                    throw response.error;
+                }
+                this.props.history.push('/');
+            })
+            .catch((error) => {
+                console.log("ERROR")
+            });
+
+    };
+    toogleEditModal = () => {
+        this.setState({
+            openeditTask: !this.state.openeditTask
+        });
+    };
+    saveTask = (editedTask) => {
+        fetch(`http://localhost:3001/task/${editedTask._id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(editedTask)
+        })
+            .then((res) => res.json())
+            .then(response => {
+                if (response.error) {
+                    throw response.error;
+                }
+
+                this.setState({
+                    singletask: response,
+                    openeditTask: false,
+                });
+            })
+            .catch((error) => {
+                console.log("ERROR")
+            });
+    };
+
+
     render() {
-        console.log(this.state)
-        const { singletask } = this.state
+        const { singletask, openeditTask } = this.state
         return (
             <>
                 {!!singletask ?
-                    <div>
-                        <h2>{singletask.title} </h2>
-                        <p> Description: {singletask.description} </p>
-                        <p> Date: {formatDate(singletask.date)} </p>
-                        <p> Created at: {formatDate(singletask.created_at)} </p>
-                    </div> :
-                    <Spinner/>}
+                    <Container>
+                        <Row className='justify-content-center text-center'>
+                            <Col xs={12} sm={10} md={8}>
+                                <div className={styles.singletask}>
+                                    <h2>{singletask.title} </h2>
+                                    <p className={styles.desc}> Description: {singletask.description} </p>
+                                    <p> Date: {formatDate(singletask.date)} </p>
+                                    <p> Created at: {formatDate(singletask.created_at)} </p>
+                                    <Button
+                                        variant="primary"
+                                        className={styles.button}
+                                        onClick={this.toogleEditModal}
+                                    >
+                                        <FontAwesomeIcon icon={faEdit} />
+                                    </Button>
+                                    <Button
+                                        className={styles.button}
+                                        variant="danger"
+                                        onClick={this.onRemove}
+                                    >
+                                        <FontAwesomeIcon icon={faTrash} />
+                                    </Button>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                    : <Spinner />}
+                { openeditTask &&
+                    <EditTaskModal
+                        data={singletask}
+                        onSave={this.saveTask}
+                        onClose={this.toogleEditModal}
+                    />
+                }
 
-                    
             </>
         )
     }
